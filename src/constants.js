0f1a4f69149e2e75c79f51e733a2603097e341d0
Utkarsh_Agrawal <utkarshagrawal45650@gmail.com>
2026-07-25 23:23:19 +0530

    feat: add constants for primary, secondary, and tertiary countdown dates

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const PRIMARY_DATE = new Date().getTime() + 15000;
-export const SECONDARY_DATE = new Date().getTime() + 30000;
-export const TERTIARY_DATE = new Date().getTime() + 45000;//'2026-08-03T12:00:00+05:30'
+export const PRIMARY_DATE = new Date('2026-07-27T12:00:00+05:30').getTime() + 15000;
+export const SECONDARY_DATE = new Date('2026-08-03T12:00:00+05:30').getTime() + 30000;
+export const TERTIARY_DATE = new Date('2026-08-06T12:00:00+05:30').getTime() + 45000;
