a9be5216a3a5f1882b5458fa641e8e9f5d1b967e
Utkarsh_Agrawal <utkarshagrawal45650@gmail.com>
2026-08-04 09:09:05 +0530

    feat: add target countdown dates to constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const PRIMARY_DATE = new Date('2026-07-27T12:00:00+05:30').getTime();
 export const SECONDARY_DATE = new Date('2026-08-03T12:00:00+05:30').getTime();
-export const TERTIARY_DATE = new Date('2026-08-06T12:00:00+05:30').getTime();
+export const TERTIARY_DATE = new Date('2026-08-10T12:00:00+05:30').getTime();
